24001a6a1e968d8923f7bbc2e231adb4b3bbcc21
schwarzwaldsvd <svartskog@gmail.com>
2020-12-28 22:37:59 -0500

    small changes

---
+++ 07_iterables/gen2.py
@@ -0,0 +1,2 @@
+hundred_squares = (x*x for x in range(1, 100))
+print(list(hundred_squares))
--- 11_leetcode/001_twoSum2.py
+++ 11_leetcode/001_twoSum2.py
@@ -1,5 +1,5 @@
 """Example:
-    nums = [1, 3, 11, 2, 7]
+    A = [1, 3, 11, 2, 7]
     target = 9
     return : [3, 4]
 
@@ -14,21 +14,43 @@ nums[i], i
 
 
 class Solution:
-    def twoSum(self, nums, target):
-        if len(nums) <= 1:
+    def twoSum(self, A, target):
+        if len(A) <= 1:
             return []
         compliments = {}
-        for i in range(len(nums)):
-            if nums[i] in compliments:
-                return [compliments[nums[i]], i]
+        for i in range(len(A)):
+            if A[i] in compliments:
+                return [compliments[A[i]], i]
             else:
-                compliments[target - nums[i]] = i
+                compliments[target - A[i]] = i
+
+    def twoSumSorted(self, A, target):
+        A.sort()
+        i = 0
+        j = len(A) - 1
+        while i < j:
+            if A[i] + A[j] == target:
+                return [i, j]
+            elif A[i] + A[j] < target:
+                i += 1
+            else:
+                j -= 1
+        return []
 
 
 s = Solution()
+"""
 print(s.twoSum([1, 3, 11, 2, 7], 9))  # Output: [3,4]
 print(s.twoSum([2, 7, 11, 15], 9))  # Output: [0,1]
 print(s.twoSum([3, 2, 4], 6))  # Output: [1,2]
 print(s.twoSum([3, 3], 6))  # Output: [0,1]
 print(s.twoSum([], 27))  # Output: []
 print(s.twoSum([2, 7, 11, 15], 26))  # Output: [2, 3]
+"""
+
+print(s.twoSumSorted([1, 2, 3, 7, 11], 9))  # Output: [1, 3]
+print(s.twoSumSorted([2, 7, 11, 15], 9))  # Output: [0,1]
+print(s.twoSumSorted([2, 3, 4], 6))  # Output: [0,2]
+print(s.twoSumSorted([3, 3], 6))  # Output: [0,1]
+print(s.twoSumSorted([], 27))  # Output: []
+print(s.twoSumSorted([2, 7, 11, 15], 26))  # Output: [2, 3]
