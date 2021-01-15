e87089b72d82556124003da6ad4e07fa4abe3aa0
schwarzwaldsvd <svartskog@gmail.com>
2021-01-14 20:54:03 -0500

    Added size property to Linked List

--- 12_dev/data_structures/linked_list.py
+++ 12_dev/data_structures/linked_list.py
@@ -7,13 +7,14 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = self.head
-        self.size = 0 # TO DO: to implement
+        self.size = 0
 
 
     def insert_at_beginning(self, data):
         if self.head is None:
             self.head = Node(data, None)
             self.tail = self.head
+            self.size += 1
             return
         
         itr = self.head
@@ -23,12 +24,14 @@ class LinkedList:
 
         node = Node(data, self.head)
         self.head = node
+        self.size += 1
 
 
     def insert_at_end(self, data):
         if self.head is None:
             self.head = Node(data, None)
             self.tail = self.head
+            self.size += 1
             return
         
         itr = self.head
@@ -37,6 +40,7 @@ class LinkedList:
         
         itr.next = Node(data, None)
         self.tail = itr.next
+        self.size += 1
 
 
     def print(self):
@@ -77,11 +81,12 @@ class LinkedList:
         return count
 
     def remove_at(self, index):
-        if index < 0 or index >= self.get_length():
+        if index < 0 or index >= self.size:
             raise Exception(f"Index '{index}' is not a valid index")
         
         if index == 0: # trying to remove the head
             self.head = self.head.next
+            self.size -= 1
             return
         
         count = 0
@@ -90,12 +95,13 @@ class LinkedList:
             if count == index - 1:
                 itr.next = itr.next.next
                 self.tail = itr
+                self.size -= 1
                 break
             itr = itr.next
             count += 1
     
     def insert_at(self, index, data):
-        if index < 0 or index >= self.get_length():
+        if index < 0 or index >= self.size:
             raise Exception(f"Index '{index}' is not a valid index")
         
         if index == 0:
@@ -108,6 +114,7 @@ class LinkedList:
                 if count == index - 1:
                     node = Node(data, itr.next)
                     itr.next = node
+                    self.size += 1
                     break
 
                 itr = itr.next
@@ -137,18 +144,19 @@ class LinkedList:
         
         if self.head.data == data_after:
             self.head.next = Node(data_to_insert, self.head.next)
+            self.size += 1
             return
         
         index = 0
-        length = self.get_length()
         itr = self.head
         while itr:
             if itr.data == data_after:
-                if index < length - 1:
+                if index < self.size - 1:
                     itr.next = Node(data_to_insert, itr.next)
                 else:  # when index == length - 1:
                     itr.next = Node(data_to_insert, None)
                     self.tail = itr.next
+                self.size += 1
                 break
             index += 1
             itr = itr.next
@@ -161,58 +169,66 @@ class LinkedList:
         
         if self.head.data == data:
             self.head = self.head.next
+            self.size -= 1
             return
-      
+
+        index = 0      
         itr = self.head
         while itr.next:
             if itr.next.data == data:
                 itr.next = itr.next.next
-                self.tail = itr.next.next.next # keeps adding next??
-                #self.tail = itr # when index = length - 1
+                if index == self.size - 2:
+                    self.tail = itr
+                self.size -= 1
                 break
             itr = itr.next
+            index += 1
 
 
 if __name__ == '__main__':
     ll = LinkedList()
-    # ll.insert_at_beginning(1)
-    # ll.insert_at_beginning(2)
-    # ll.insert_at_end(3)
-    # ll.insert_at_end(4)
-    # ll.print()
-    # print("head:", ll.head.data, ", tail:", ll.tail.data)
-    # ll.insert_values([10,20,30,40,50], False)
-    # ll.print()
-    # print("head:", ll.head.data, ", tail:", ll.tail.data)
-    # ll.insert_values_reversed([22,33,44,55,77], False)
-    # ll.print()
-    # print("head:", ll.head.data, ", tail:", ll.tail.data)
+    ll.insert_at_beginning(1)
+    ll.insert_at_beginning(2)
+    ll.insert_at_end(3)
+    ll.insert_at_end(4)
+    ll.insert_at(1, "cherry")
+    ll.print()
+    print(ll.size)
+    print("head:", ll.head.data, ", tail:", ll.tail.data, ", size:", ll.size)
+    ll.insert_values([10,20,30,40,50], False)
+    ll.print()
+    print("head:", ll.head.data, ", tail:", ll.tail.data, ", size:", ll.size)
+    ll.insert_values_reversed([22,33,44,55,77], False)
+    ll.print()
+    print("head:", ll.head.data, ", tail:", ll.tail.data, ", size:", ll.size)
     
-    # print(ll.find_index_by_value(79))
-    # ll.insert_after_value(79,99)
-    # ll.print()
+    print(ll.find_index_by_value(79))
+    ll.insert_after_value(79,99)
+    ll.print()
 
     ll.insert_values_reversed(["pear","grapefruit","carrot","potato","jackfruit"], False)
     ll.insert_values(["banana","mango","grapes","orange","figs"], False)
-    # print("Index for 'figs' is ", ll.find_index_by_value("figs"))
-    # ll.insert_after_value("mango","apple") # insert apple after mango
+    print("Index for 'figs' is ", ll.find_index_by_value("figs"))
+    ll.insert_after_value("mango","apple") # insert apple after mango
     ll.print()
-    print("head:", ll.head.data, ", tail:", ll.tail.data)
-    # ll.remove_by_value("orange") # remove orange from linked list
+    print("head:", ll.head.data, ", tail:", ll.tail.data, ", size:", ll.size)
+    ll.remove_by_value("orange") # remove orange from linked list
     print(ll.get_length())
-    # ll.remove_by_value("figs")
-    # ll.insert_at(8, "ice-cream")
-    # print(ll.find_index_by_value("figs"))
-    # ll.insert_after_value("jackfruit", "ice-cream")
-    ll.remove_by_value("mango")
+    ll.remove_by_value("figs")
+    ll.insert_at(8, "ice-cream")
+    print(ll.find_index_by_value("figs"))
+    ll.insert_after_value("jackfruit", "ice-cream")
+    ll.remove_by_value("figs")
+    ll.remove_by_value("orange")
     ll.print()
-    print("head:", ll.head.data, ", tail:", ll.tail.data)
+    print("head:", ll.head.data, ", tail:", ll.tail.data, ", size:", ll.size)
     print(ll.get_length())
 
-    # ll.remove_by_value("figs")
-    # ll.print()
-    # ll.remove_by_value("banana")
-    # ll.remove_by_value("mango")
-    # ll.remove_by_value("apple")
-    # ll.remove_by_value("grapes")
-    # ll.print()
+    ll.remove_by_value("figs")
+    ll.print()
+    ll.remove_by_value("banana")
+    ll.remove_by_value("mango")
+    ll.remove_by_value("apple")
+    ll.remove_by_value("grapes")
+    ll.print()
+    print("head:", ll.head.data, ", tail:", ll.tail.data, ", size:", ll.size)
